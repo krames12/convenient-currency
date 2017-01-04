a2813d2aba119cddc1cfcfd412acd1ca9a9415ac
Nick Kramer <krames12@gmail.com>
2017-01-04 11:25:56 -0500

    api request works. formatted data as well

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const request = require('request');
 const express = require('express');
 const app = express();
 
+//Using USD as a base for now. Would like to have the base be dynamic but we'll get there maybe...
+const apiUrl = "https://api.fixer.io/latest?base=USD"
+
 const port = process.env.PORT || 8080;
 //sets EJS as the templating language
 app.set('view-engine', 'ejs');
@@ -12,3 +15,25 @@ app.use('/public', express.static('public'));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//request for currency data
+function getConversionData() {
+  request(apiUrl, (err, res, body) => {
+    if(!err && res.statusCode == 200) {
+      console.log('status', res.statusCode);
+      var parsedBody = JSON.parse(body);
+      return {
+        "status": true,
+        "date": parsedBody.date,
+        "rates": parsedBody.rates
+      };
+    }
+    else if(err || res.statusCode !== 200) {
+      return {
+        "status": false
+      }
+    }
+  });
+}
+
+getConversionData();
