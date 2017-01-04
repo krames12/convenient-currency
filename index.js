0f850817a089c0e479322b3b1fa3d681189ecbb2
Nick Kramer <krames12@gmail.com>
2017-01-04 19:14:08 +0000

    ditched request package. using promise with http.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const request = require('request');
+const http = require('http');
 const express = require('express');
 const app = express();
 
 //Using USD as a base for now. Would like to have the base be dynamic but we'll get there maybe...
-const apiUrl = "https://api.fixer.io/latest?base=USD"
+const apiUrl = "http://api.fixer.io/latest?base=USD"
 
 const port = process.env.PORT || 8080;
 //sets EJS as the templating language
@@ -13,30 +13,45 @@ app.use('/public', express.static('public'));
 
 //sets default render route
 app.get('/', (req, res) => {
-  let currencyData = getConversionData();
-  res.render('index', {data: currencyData});
+  getConversionData().then((data) => { res.render('index', {info: data}); }).catch((err) => { console.log(err); });
 });
 
 //request for currency data
 function getConversionData() {
-  request(apiUrl, (err, res, body) => {
-    if(!err && res.statusCode == 200) {
-      console.log('fixer status', res.statusCode);
-      var parsedBody = JSON.parse(body);
-      return {
-        "status": true,
-        "date": parsedBody.date,
-        "rates": parsedBody.rates
-      };
-    }
-    else if(err || res.statusCode !== 200) {
-      return {
-        "status": false
-      }
-    }
+  return new Promise((resolve, reject) => {
+    http.get(apiUrl, (res) => {
+      res.setEncoding('utf8');
+      
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
+      
+      //variable for the body of the response to reside in.
+      var body = '';
+      
+      //upon recieving of data, add it to the body variable
+      res.on('data', (data) => {
+        body += data;
+      });
+      
+      //once completed, resolve or reject the promise based on status code
+      res.on('end', () => {
+        try {
+          var parsed = JSON.parse(body);
+        } catch (err) {
+          return reject(err);
+        }
+        if(res.statusCode !== 404 || 504) {
+          resolve(parsed);
+        } else {
+          reject();
+        }
+      //handeling any error in the process
+      }).on('error', (err) => {
+        reject(err);
+      })
+    });
   });
 }
-
 //necessary for server to actually run
 app.listen(port, () => {
   console.log('app is now listening to port', port);
